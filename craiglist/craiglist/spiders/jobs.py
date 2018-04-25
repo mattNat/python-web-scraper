a307fe5b510e56a0821c0f7d2ac30214904d8a26
mattNat <mattsanpedro76@gmail.com>
2018-04-25 16:39:15 -0700

    start url crawling for compensation and type

--- craiglist/craiglist/spiders/jobs.py
+++ craiglist/craiglist/spiders/jobs.py
@@ -5,7 +5,7 @@ import scrapy
 class JobsSpider(scrapy.Spider):
     name = 'jobs'
     allowed_domains = ['sandiego.craigslist.org']
-    start_urls = ['https://sandiego.craigslist.org/search/ofc']
+    start_urls = ['https://sandiego.craigslist.org/search/web']
 
     def parse(self, response):
         # listings = response.xpath('//a[@class="result-title hdrlnk"]/text()').extract()
@@ -19,11 +19,27 @@ class JobsSpider(scrapy.Spider):
             link = listing.xpath('.//a[@class="result-title hdrlnk"]/@href').extract_first()
             text = listing.xpath('.//a[@class="result-title hdrlnk"]/text()').extract_first()
 
-            yield {
-                'date': date,
-                'link': link,
-                'text': text
-            }
+            compensation = response.xpath('//*[@class="attrgroup"]/span[1]/b/text()').extract_first()
+            type = response.xpath('//*[@class="attrgroup"]/span[2]/b/text()').extract_first()
+
+            # yield {
+            #     'date': date,
+            #     'link': link,
+            #     'text': text
+            # }
+
+            yield scrapy.Request(
+                link, 
+                callback=self.parse_listing, 
+                meta={'date': date, 'link': link, 'text': text}
+                )
+
         next_page_url = response.xpath('//a[text()="next > "]/@href').extract_first()
         if next_page_url:
             yield scrapy.Request(response.urljoin(next_page_url), callback=self.parse)
+    
+    def parse_listing(self, response):
+        date = response.meta['date']
+        link = response.meta['link']
+        text = response.meta['text']
+        
