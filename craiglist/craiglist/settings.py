72d5b3e2ef355f191d8a71032793022f0e6b7ab5
mattNat <mattsanpedro76@gmail.com>
2018-04-25 15:17:41 -0700

    crawl Craigslist jobs including next page functionality

--- craiglist/craiglist/settings.py
+++ craiglist/craiglist/settings.py
@@ -16,7 +16,7 @@ NEWSPIDER_MODULE = 'craiglist.spiders'
 
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = 'craiglist (+http://www.yourdomain.com)'
+USER_AGENT = 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:40.0) Gecko/20100101 Firefox/40.1'
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = True
@@ -27,7 +27,7 @@ ROBOTSTXT_OBEY = True
 # Configure a delay for requests for the same website (default: 0)
 # See https://doc.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-#DOWNLOAD_DELAY = 3
+DOWNLOAD_DELAY = 3
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN = 16
 #CONCURRENT_REQUESTS_PER_IP = 16
@@ -88,3 +88,31 @@ ROBOTSTXT_OBEY = True
 #HTTPCACHE_DIR = 'httpcache'
 #HTTPCACHE_IGNORE_HTTP_CODES = []
 #HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
+
+# # proxy setup
+# # Retry many times since proxies often fail
+# RETRY_TIMES = 10
+# # Retry on most error codes since proxies fail for different reasons
+# RETRY_HTTP_CODES = [500, 503, 504, 400, 403, 404, 408]
+
+# DOWNLOADER_MIDDLEWARES = {
+#     'scrapy.downloadermiddlewares.retry.RetryMiddleware': 90,
+#     'scrapy_proxies.RandomProxy': 100,
+#     'scrapy.downloadermiddlewares.httpproxy.HttpProxyMiddleware': 110,
+# }
+
+# # Proxy list containing entries like
+# # http://host1:port
+# # http://username:password@host2:port
+# # http://host3:port
+# # ...
+# PROXY_LIST = '/media/merlin/DarthVader/list.txt'
+
+# # Proxy mode
+# # 0 = Every requests have different proxy
+# # 1 = Take only one proxy from the list and assign it to every requests
+# # 2 = Put a custom proxy to use in the settings
+# PROXY_MODE = 0
+
+# # If proxy mode is 2 uncomment this sentence :
+# #CUSTOM_PROXY = "http://host1:port"
--- craiglist/craiglist/spiders/jobs.py
+++ craiglist/craiglist/spiders/jobs.py
@@ -5,7 +5,25 @@ import scrapy
 class JobsSpider(scrapy.Spider):
     name = 'jobs'
     allowed_domains = ['sandiego.craigslist.org']
-    start_urls = ['http://sandiego.craigslist.org/']
+    start_urls = ['https://sandiego.craigslist.org/search/ofc']
 
     def parse(self, response):
-        pass
+        # listings = response.xpath('//a[@class="result-title hdrlnk"]/text()').extract()
+        listings = response.xpath('//li[@class="result-row"]')
+        for listing in listings:
+            # # print(listing)
+            # yield {
+            #     'Listing': listing
+            # }
+            date = listing.xpath('.//*[@class="result-date"]/@datetime').extract_first()
+            link = listing.xpath('.//a[@class="result-title hdrlnk"]/@href').extract_first()
+            text = listing.xpath('.//a[@class="result-title hdrlnk"]/text()').extract_first()
+
+            yield {
+                'date': date,
+                'link': link,
+                'text': text
+            }
+        next_page_url = response.xpath('//a[text()="next > "]/@href').extract_first()
+        if next_page_url:
+            yield scrapy.Request(response.urljoin(next_page_url), callback=self.parse)
